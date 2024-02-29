e7968f841df7435e95d1bc026738521058147729
Alok290 <79790013+Alok290@users.noreply.github.com>
2024-02-29 18:30:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function fibonacci(num) {
-// your code here
-	if(num<=2) return num-1;
-	return fibonacci(n-1)+fibonacci(n-2);
-}
+function fibonacci(n) {
+  // write code here
+    if(n==1) return 0;
+    if (n==2 ) return 1;
+
+    return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+module.exports = fibonacci;
+
 
 module.exports = fibonacci;
